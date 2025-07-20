bc54810a87a07d7306694f7325b7c718b451be96
Sriraj Palakurthi <srirajdev@Srirajs-Laptop.local>
2025-07-20 23:21:24 +0530

    Resolved Prod errors

--- src/components/floating-chat-window.tsx
+++ src/components/floating-chat-window.tsx
@@ -152,9 +152,9 @@ export const FloatingChatWindow = () => {
 
   return (
     <motion.div
-      initial={{ opacity: 0, y: 100 }}
-      animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: 100 }}
+      // initial={{ opacity: 0, y: 100 }}
+      // animate={{ opacity: 1, y: 0 }}
+      // exit={{ opacity: 0, y: 100 }}
       transition={{ duration: 0.3 }}
       className="flex flex-col w-[450px] h-[75vh] rounded-xl shadow-lg bg-primary backdrop-blur-lg overflow-hidden">
       {/* Header */}
